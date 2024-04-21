047b7b8a14ac862f4af25f4373c537103377e066
Mateo Kladarić <52569856+mateokladaric@users.noreply.github.com>
2024-04-21 13:15:45 +0200

    Add files via upload

--- Assets/Scripts/EditorController.cs
+++ Assets/Scripts/EditorController.cs
@@ -5,6 +5,7 @@ public class EditorController : MonoBehaviour
 	public GameObject squarePrefab; // The square prefab
 	public Transform customShapeParent; // The parent of the custom shape
 	public int blockCount = 5; // The number of blocks
+	public int maxCustomShapes = 7; // The maximum number of blocks
 	private Transform fallingSquare; // The square that is falling
 	private bool startedFalls = false; // Has it started falling
 	private float fallTime; // The timer on when it should fall 
@@ -18,7 +19,7 @@ public class EditorController : MonoBehaviour
 	void Update()
 	{
 		// if the user presses any of the arrow keys
-		if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+		if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
 		{
 			// If the game has not started falling
 			if (!startedFalls)
@@ -54,10 +55,10 @@ public class EditorController : MonoBehaviour
 					}
 				}
 				// If the user presses the down arrow key
-				if (Input.GetKeyDown(KeyCode.DownArrow))
+				if (Input.GetKey(KeyCode.DownArrow))
 				{
 					// reduce the fall time
-					fallTime = 0;
+					fallTime -= 0.1f;
 				}
 			}
 		}
@@ -75,8 +76,26 @@ public class EditorController : MonoBehaviour
 			// if block count is 0
 			else if (blockCount == 0)
 			{
-				// Set the started falls to false
+				// add CustomShape to GameSettings
+				GameObject.Find("gameSettings").GetComponent<GameSettings>().addTetromino(customShapeParent.gameObject);
+				// decrement maxCustomShapes
+				maxCustomShapes--;
+				// if maxCustomShapes is 0
+				if (maxCustomShapes == 0)
+				{
+					// load the game scene
+					//UnityEngine.SceneManagement.SceneManager.LoadScene("8bit");
+				}
+				// set block count to 5
+				blockCount = 5;
+				// set startedFalls to false
 				startedFalls = false;
+				// push the custom shape down
+				customShapeParent.position = new Vector3(10, 0, 0);
+				// set new custom shape parent
+				customShapeParent = new GameObject().transform;
+				// set name of new custom shape parent
+				customShapeParent.name = "CustomShape" + (5 - maxCustomShapes);
 			}
 			// if there is a falling square
 			else
